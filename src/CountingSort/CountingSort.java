06fa834c89b1a7e612355a6b18ee9ca57c2401a4
JayantDwivedi <jayantdwivedi19@gmail.com>
2020-08-09 14:34:10 +0530

    Counting Sort added

---
+++ src/CountingSort/CountingSort.java
@@ -0,0 +1,44 @@
+package CountingSort;
+
+import java.util.Arrays;
+
+public class CountingSort {
+    public static void main(String[] args) {
+        int[] arr = {1,4,1,2,7,5,2};            // choose element from 0 to 9
+        System.out.println("Unsorted --> " + Arrays.toString(arr));
+        sort(arr);
+        System.out.println("Sorted --> " + Arrays.toString(arr));
+    }
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        int[] newArr = new int[n];     // new Array to store output
+        int[] count = new int[9];       // array to count the occurance of element
+
+        // initialise count with 0
+        for (int i = 0; i < 9; i++) {
+            count[i] = 0;
+        }
+
+        // count the occurrance of every element
+        for (int i = 0; i < n; i++) {
+            ++count[arr[i]];
+        }
+
+        // change the count[i] now contain actual position of this character
+        // in output array
+        for (int i = 1; i <= 9; i++) {
+            count[i] = count[i - 1];
+        }
+
+        // build output array
+        for (int i = n-1 ; i > 0; i--) {
+            newArr[count[arr[i]] - 1] = arr[i];
+            --count[i];
+        }
+
+        // because we void
+        for (int i = 0; i < n; i++) {
+            arr[i] = newArr[i];
+        }
+    }
+}
--- src/LinkedList/Practice/LinkList.java
+++ src/LinkedList/Practice/LinkList.java
@@ -1,10 +1,44 @@
 package LinkedList.Practice;
 
 class LinkList {
-    Node1 head = null;
-    public static void insert(int data) {
-        Node1 node = new Node1(data);
-        
 
+    Node1 head = null;
+
+    // insertion from beginning
+    public void insert(int data) {
+        Node1 node = new Node1(data);
+        node.next = head;
+        head = node;
+    }
+
+    // insertion from last
+    public void insertLast(int data) {
+        Node1 node = new Node1(data);
+        Node1 temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = node;
+    }
+
+    public void display() {
+        Node1 temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkList llist = new LinkList();
+        llist.insert(1);
+        llist.insert(2);
+        llist.insert(3);
+        llist.insertLast(4);
+        llist.insertLast(5);
+        llist.insertLast(6);
+
+        llist.display();
     }
 }
