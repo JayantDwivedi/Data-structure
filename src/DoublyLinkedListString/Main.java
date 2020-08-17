92c1a89207411daacd5a859192ec349f84cf0188
JayantDwivedi <jayantdwivedi19@gmail.com>
2020-08-17 13:32:39 +0530

    some changes

--- src/DoublyLinkedListString/Main.java
+++ src/DoublyLinkedListString/Main.java
@@ -3,6 +3,9 @@ package DoublyLinkedListString;
 public class Main {
     public static void main(String[] args) {
         Implementation obj = new Implementation();
+
+
+
         obj.insertFirst("Jayant");
         obj.insertFirst("Kartik");
         obj.insertFirst("haiek");
@@ -10,6 +13,7 @@ public class Main {
         obj.insertLast(10);
         obj.insertLast(10.34F);
         obj.insertLast(23L);
+
 
         obj.display();
     }
