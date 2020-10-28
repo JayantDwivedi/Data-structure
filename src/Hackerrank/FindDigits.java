65055888292280750ea8ab7cc7d7abda4a45e4bc
JayantDwivedi <jayantdwivedi19@gmail.com>
2020-10-28 22:51:37 +0530

    added find digit

---
+++ src/Hackerrank/FindDigits.java
@@ -0,0 +1,25 @@
+package Hackerrank;
+
+import java.util.Scanner;
+
+public class FindDigits {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int test = scan.nextInt();
+        for (int i = 0; i < test; i++) {
+            int num = scan.nextInt();
+            int n = num;
+            int count = 0;
+            int rem = 0;
+            while (num > 0) {
+                rem = num % 10;
+                System.out.println("n : " + rem);
+                if ((rem != 0) && (n % rem == 0)) {
+                    count++;
+                }
+                num = num / 10;
+            }
+            System.out.println("Count : " + count);
+        }
+    }
+}
